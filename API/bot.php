d01c99ed2d6df7079ae73044a924157cbb02f9b5
Gleb Rybak <bubusyaka008@gmail.com>
2019-01-20 22:43:25 +0200

    Update bot.php

--- API/bot.php
+++ API/bot.php
@@ -1,53 +1,27 @@
-<?php 
-
-if (!isset($_REQUEST)) { 
-return; 
-} 
-
-//Строка для подтверждения адреса сервера из настроек Callback API 
-$confirmation_token = 'c286fa13'; 
-
-//Ключ доступа сообщества 
-$token = 'd550e028133aa6fe6baf8e3fc7cce48800de1ed7f0989fe7c9857833af39ee78221e3bdc2f3504410d9e0'; 
-
-//Получаем и декодируем уведомление 
-$data = json_decode(file_get_contents('php://input')); 
-
-//Проверяем, что находится в поле "type" 
-switch ($data->type) { 
-//Если это уведомление для подтверждения адреса... 
-case 'confirmation': 
-//...отправляем строку для подтверждения 
-echo $confirmation_token; 
-break; 
-
-//Если это уведомление о новом сообщении... 
-case 'message_new': 
-//...получаем id его автора 
-$user_id = $data->object->user_id; 
-//затем с помощью users.get получаем данные об авторе 
-$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.0")); 
-
-//и извлекаем из ответа его имя 
-$user_name = $user_info->response[0]->first_name; 
-
-//С помощью messages.send отправляем ответное сообщение 
-$request_params = array( 
-'message' => "Hello, {$user_name}!", 
-'user_id' => $user_id, 
-'access_token' => $token, 
-'v' => '5.0' 
-); 
-
-$get_params = http_build_query($request_params); 
-
-file_get_contents('https://api.vk.com/method/messages.send?'. $get_params); 
-
-//Возвращаем "ok" серверу Callback API 
-
-echo('ok'); 
-
-break; 
-
-} 
+<?php
+if (!isset($_REQUEST)) { return; }
+ 
+$confirmation_token = 'c286fa13';
+$token = 'd550e028133aa6fe6baf8e3fc7cce48800de1ed7f0989fe7c9857833af39ee78221e3bdc2f3504410d9e0';
+ 
+$data = json_decode(file_get_contents('php://input'));
+switch ($data->type) {
+    case 'confirmation': echo $confirmation_token; break;
+    case 'message_new':
+        $user_id = $data->object->user_id;
+        $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.0"));
+        $user_name = $user_info->response[0]->first_name;
+ 
+        $request_params = array(
+            'message' => "Hello, {$user_name}!",
+            'user_id' => $user_id,
+            'access_token' => $token,
+            'v' => '5.0'
+        );
+        $get_params = http_build_query($request_params);
+        file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
+ 
+        echo('ok');
+    break;
+}
 ?>
